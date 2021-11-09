8b8e9038309d2730a6b5c7012bcf7c167a945adb
Hyerim Yeom <hyerimyeom@Hyerimui-MacBookPro.local>
2021-11-09 17:55:13 +0900

    #2 Todolist 컴포넌트 생성
    - 추후에 반복될 일이 많을 것 같아 생성
    - main.css로 뺌

---
+++ todolist/src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="row">
+        <span class="cell col1"> {{ todo.user }} </span>
+        <span class="cell col2"> {{ todo.title }} </span>
+        <span class="cell col3"> {{ todo.endDate }} </span>
+        <span class="cell col4"> {{ todo.done }}  </span>
+    </div>    
+</template>
+
+<script>
+export default {
+    name : 'TodoList',
+    props : { 
+        todo : Object,
+        
+    },
+}
+</script>
+
+<style>
+
+</style>
