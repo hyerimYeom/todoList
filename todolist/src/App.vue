587f2b6980490fd718b26bd34ef824b3fbaecb69
Hyerim Yeom <hyerimyeom@Hyerimui-MacBookPro.local>
2021-11-09 17:09:34 +0900

    #2 화면 샘플
    - 데이터 .js 샘플파일
    - todolist 테이블 생성

---
+++ todolist/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <div>To-Do list</div>
+    <!-- 표 연습 -->
+   <div id="table">
+    <div class="row">
+    <span class="cell col1">작성자</span>
+    <span class="cell col2">내용 </span>
+    <span class="cell col3">마감일</span>
+    <span class="cell col4">done</span>
+    </div>
+    <div class="row"> 
+    <span class="cell col1"> {{todolist[0].user}} </span>
+    <span class="cell col2"> todo list 프로젝트 오늘 완성하기</span>
+    <span class="cell col3"> 2021-11-09</span>
+    <span class="cell col4"> 아이콘 필요 </span>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import todolist from './assets/todolist_sample.js'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      todolist : todolist
+    }
+  },
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#table {display: table; width: 100%;}
+.row {display: table-row;}
+.cell {display: table-cell; padding: 3px; border-bottom: 1px solid #DDD;}
+.col1 {width: 20%;}
+.col2 {width: 20%;}
+.col3 {width: 40%;}
+.col4 {width: 20%;}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todolist/src/assets/todolist_sample.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 0,
+    user: "HR",
+    title: "todo list 오늘 다 완성 하기 ",
+    startDate: "2021-11-09 00:00:00",
+    endDate: "2021-11-09 00:00:00",
+    done: false,
+  },
+  {
+    id : 1,
+    user: "HR",
+    title: "todo list 오늘 다 완성 하기 1",
+    startDate: "2021-11-09 00:00:00",
+    endDate: "2021-11-09 00:00:00",
+    done: false,
+  },
+  {
+    id: 2,
+    user: "HR",
+    title: "todo list 오늘 다 완성 하기 2",
+    startDate: "2021-11-09 00:00:00",
+    endDate: "2021-11-09 00:00:00",
+    done: false,
+  },
+];
